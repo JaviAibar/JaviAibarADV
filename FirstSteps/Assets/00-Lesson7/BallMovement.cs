9994c2627ad72361a069af3aa5d031599a68ff13
Javi <jaaiar@inf.upv.es>
2021-03-26 20:56:34 +0100

    Code

--- FirstSteps/Assets/00-Lesson7/BallMovement.cs
+++ FirstSteps/Assets/00-Lesson7/BallMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     float timer;
+    float m_Speed = 2f;
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
@@ -16,18 +17,25 @@ public class BallMovement : MonoBehaviour
     void FixedUpdate()
     {
         timer += Time.deltaTime;
+
+        Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //Apply the movement vector to the current position, which is
+        //multiplied by deltaTime and speed for a smooth MovePosition
+        m_Rigidbody.MovePosition(transform.position + m_Input * Time.deltaTime * m_Speed);
     }
 
-    float val = 0f;
     // Update is called once per frame
 
     void OnTriggerEnter(Collider other)
     {
-    timer = 0;
+        timer = 0;
+        print("enter");
     }
 
     void OnTriggerStay(Collider other)
     {
-        other.transform.GetComponent<Animator>().SetFloat("Colour", (timer / 600f));
+        other.transform.GetComponent<Animator>().SetFloat("Colour", Mathf.Clamp01(timer / 600f));
+        print("stay");
     }
 }
