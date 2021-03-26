8b76a75df4f0b48916f5cfada9cebc426458f93c
Javi <jaaiar@inf.upv.es>
2021-03-26 20:45:27 +0100

    Portfolio

---
+++ FirstSteps/Assets/00-Lesson7/BallMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallMovement : MonoBehaviour
+{
+    float timer;
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    Rigidbody m_Rigidbody;
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+    }
+
+    float val = 0f;
+    // Update is called once per frame
+
+    void OnTriggerEnter(Collider other)
+    {
+    timer = 0;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        other.transform.GetComponent<Animator>().SetFloat("Colour", (timer / 600f));
+    }
+}
