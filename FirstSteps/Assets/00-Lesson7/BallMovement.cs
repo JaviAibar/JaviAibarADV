aaca6cce1e7c27d5cd0edf0c26a91e8790b5b35f
Javi <jaaiar@inf.upv.es>
2021-04-06 01:01:43 +0200

    Problems of project 3 solved

--- FirstSteps/Assets/00-Lesson7/BallMovement.cs
+++ FirstSteps/Assets/00-Lesson7/BallMovement.cs
@@ -2,40 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class BallMovement : MonoBehaviour
 {
-    float timer;
-    float m_Speed = 2f;
-    void Start()
-    {
-        m_Rigidbody = GetComponent<Rigidbody>();
-    }
+    public float m_Speed = 5f;
 
-    Rigidbody m_Rigidbody;
     // Start is called before the first frame update
     void FixedUpdate()
     {
-        timer += Time.deltaTime;
-
         Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        //Apply the movement vector to the current position, which is
-        //multiplied by deltaTime and speed for a smooth MovePosition
-        m_Rigidbody.MovePosition(transform.position + m_Input * Time.deltaTime * m_Speed);
+        transform.Translate(m_Input*Time.deltaTime*m_Speed);
     }
 
-    // Update is called once per frame
-
-    void OnTriggerEnter(Collider other)
-    {
-        timer = 0;
-        print("enter");
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        other.transform.GetComponent<Animator>().SetFloat("Colour", Mathf.Clamp01(timer / 600f));
-        print("stay");
-    }
 }
---
+++ FirstSteps/Assets/00-Lesson7/ColourController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourController : MonoBehaviour
+{
+    float val = 0f;
+    float colourSpeed = 0.8f;
+    Animator animator;
+    bool colliding;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (val > 0 && !colliding)
+        {
+            val -= Time.deltaTime * colourSpeed;
+            animator.SetFloat("Colour", val);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (val < 1)
+        {
+            val += Time.deltaTime * colourSpeed;
+            animator.SetFloat("Colour", val);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        colliding = true;
+        animator.SetTrigger("GoDown");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        colliding = false;
+        animator.SetTrigger("GoUp");
+    }
+
+}
