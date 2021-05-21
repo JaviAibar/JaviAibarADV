24d0ee5cb015a294a0719850b96ae82b86783e46
Javi <jaaiar@inf.upv.es>
2021-05-21 15:19:12 +0200

    Added ScreenShake

---
+++ FirstSteps/Assets/00-Project4/Scripts/PirateController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateController : MonoBehaviour
+{
+    public float timer;
+    public Animator cameraAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer % 5 < 0.01f)
+        {
+            print("HIT");
+            cameraAnimator.SetTrigger("Hit");
+        }
+    }
+}
